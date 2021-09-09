e91adfb67ff0e907d9a67575757aacc41cff5da0
sxc-one <sxc-one@foxmail.com>
2021-09-09 15:03:59 +0800

    ShortLinkManagerController

--- src/main/java/top/ysxc/zfile/controller/admin/ShortLinkManagerController.java
+++ src/main/java/top/ysxc/zfile/controller/admin/ShortLinkManagerController.java
@@ -1,12 +1,14 @@
 package top.ysxc.zfile.controller.admin;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import top.ysxc.zfile.model.support.ResultBean;
 import top.ysxc.zfile.service.ShortLinkConfigService;
 
 import javax.annotation.Resource;
 
 /**
+ * 直链管理 Controller
  * @author ysxc
  * @create 2021-09-08 2:14 下午
  */
@@ -17,5 +19,18 @@ public class ShortLinkManagerController {
     @Resource
     private ShortLinkConfigService shortLinkConfigService;
 
+    @GetMapping("/link/list")
+    @ResponseBody
+    public ResultBean list(String key, String url, String dateFrom, String dateTo, Integer page, Integer limit,
+                           @RequestParam(required = false, defaultValue = "createDate") String orderBy,
+                           @RequestParam(required = false, defaultValue = "desc") String orderDirection) {
+        return ResultBean.success(shortLinkConfigService.find(key, url, dateFrom, dateTo, page, limit, orderBy, orderDirection));
+    }
 
+    @GetMapping("/link/delete/{id}")
+    @ResponseBody
+    public ResultBean deleteById(@PathVariable Integer id) {
+        shortLinkConfigService.deleteById(id);
+        return ResultBean.success();
+    }
 }
--- src/main/java/top/ysxc/zfile/service/ShortLinkConfigService.java
+++ src/main/java/top/ysxc/zfile/service/ShortLinkConfigService.java
@@ -1,10 +1,20 @@
 package top.ysxc.zfile.service;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import top.ysxc.zfile.model.entity.ShortLinkConfig;
 import top.ysxc.zfile.repository.ShortLinkConfigRepository;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author ysxc
@@ -18,5 +28,34 @@ public class ShortLinkConfigService {
 
     public ShortLinkConfig findByKey(String key) {
         return shortLinkConfigRepository.findFirstByKey(key);
+    }
+
+    public Page<ShortLinkConfig> find(String key, String url, String dateFrom, String dateTo, Integer page, Integer limit, String orderBy, String orderDirection) {
+        Sort sort = Sort.by("desc".equals(orderDirection) ? Sort.Direction.DESC : Sort.Direction.ASC, orderBy);
+        Pageable pageable = PageRequest.of(page - 1, limit, sort);
+        Specification<ShortLinkConfig> specification = (root, criteriaQuery, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (StrUtil.isNotEmpty(dateFrom) && StrUtil.isNotEmpty(dateTo)) {
+                predicates.add(criteriaBuilder.between(root.get("createDate"),
+                        DateUtil.parseDateTime(dateFrom + "00:00:00"),
+                        DateUtil.parseDateTime(dateTo + "23:59:59")));
+            }
+
+            if (StrUtil.isNotEmpty(key)) {
+                predicates.add(criteriaBuilder.like(root.get("key"), "%" + key + "%"));
+            }
+
+            if (StrUtil.isNotEmpty(url)) {
+                predicates.add(criteriaBuilder.like(root.get("url"), "%" + url + "%"));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+        return shortLinkConfigRepository.findAll(specification, pageable);
+    }
+
+    public void deleteById(Integer id) {
+        shortLinkConfigRepository.deleteById(id);
     }
 }
